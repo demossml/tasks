449559af2d109532e535bf1504ddc56078f58ca7
Dima <demossml11@gmail.com>
2024-08-25 10:53:53 +0300

    reformat 2

--- src/tasks/CarNotFoundException.java
+++ src/tasks/CarNotFoundException.java
@@ -1,8 +1,9 @@
 package tasks;
 
-public class CarNotFoundException extends Exception{
+public class CarNotFoundException extends Exception {
 
-    public CarNotFoundException(String message) {
+    public CarNotFoundException(String carId) {
+        String message = "Машина с id: " + carId + " е найден!\n";
         super(message);
     }
 }
