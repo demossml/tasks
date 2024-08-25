ef3cb1852cadd00c91fa659927478983ef47c7a6
Dima <demossml11@gmail.com>
2024-08-25 10:51:33 +0300

    reformat

--- src/tasks/Parking.java
+++ src/tasks/Parking.java
@@ -18,44 +18,44 @@ public class Parking {
     // Метод для парковки машины. Возвращает true, если удалось припарковаться
     public boolean park(String carId, int hour) throws CarIsAlreadyParkedException {
 
-            if (parkingTime.containsKey(carId)) {
-                throw new CarIsAlreadyParkedException(carId);
-            }
+        if (parkingTime.containsKey(carId)) {
+            throw new CarIsAlreadyParkedException(carId);
+        }
 
-            if (parkingTime.size() < capacity) {
-                parkingTime.put(carId, hour);
-                System.out.printf("Машина с id:%s припаркована!\n", carId);
-                return true;
-            } else {
-                System.out.println("Парковка переполнена!");
-                return false;
-            }
+        if (parkingTime.size() < capacity) {
+            parkingTime.put(carId, hour);
+            System.out.printf("Машина с id:%s припаркована!\n", carId);
+            return true;
+        } else {
+            System.out.println("Парковка переполнена!");
+            return false;
+        }
 
     }
 
     public int unpark(String carId, int hour) throws CarNotFoundException {
 
-            if (!parkingTime.containsKey(carId)) {
-                throw new CarNotFoundException(carId);
+        if (!parkingTime.containsKey(carId)) {
+            throw new CarNotFoundException(carId);
+        }
+
+        int startHour = parkingTime.get(carId);
+        int totalSum = 0;
+        for (int h = startHour; h <= hour; h++) {
+            int dayOfWeek = (h / 24) % 7;
+            boolean isWeekend = dayOfWeek == 5 || dayOfWeek == 6;
+
+            int rate = ((h % 24) >= 22 || (h % 24) < 7) ? NIGHT_RATE : DAY_RATE;
+
+            if (!isWeekend) {
+                rate /= 2;
             }
+            totalSum += rate;
 
-            int startHour = parkingTime.get(carId);
-            int totalSum = 0;
-            for (int h = startHour; h <= hour; h++){
-                    int dayOfWeek = (h / 24) % 7;
-                    boolean isWeekend = dayOfWeek == 5 || dayOfWeek == 6;
-
-                    int rate = ((h % 24) >= 22 || (h % 24) < 7) ? NIGHT_RATE : DAY_RATE;
-
-                    if (!isWeekend) {
-                        rate /= 2;
-                    }
-                    totalSum += rate;
-
-                }
-            parkingTime.remove(carId);
-            System.out.printf("Машина с ID %s убрана из парковки. Общая сумма за парковку: %d\n", carId, totalSum);
-            return totalSum;
+        }
+        parkingTime.remove(carId);
+        System.out.printf("Машина с ID %s убрана из парковки. Общая сумма за парковку: %d\n", carId, totalSum);
+        return totalSum;
 
 
     }
