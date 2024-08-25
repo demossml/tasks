54a6cc813d1f2be4407b92e9d13bcdb58cd058c0
Dima <demossml11@gmail.com>
2024-08-25 09:36:55 +0300

    Exception //

--- src/tasks/Parking.java
+++ src/tasks/Parking.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Parking {
     private static final int NIGHT_RATE = 30;  // Ставка за ночь
     private static final int DAY_RATE = 50;    // Ставка за день
-    private int capacity;
+    private int capacity; //Вместимость парковки
     private Map<String, Integer> parkingTime = new HashMap<>();
 
     public Parking(int capacity) {
@@ -13,7 +13,7 @@ public class Parking {
 
     }
 
-    // Метод для парковки машины
+    // Метод для парковки машины. Возвращает true, если удалось припарковаться
     public boolean park(String carId, int hour) {
         try {
             if (parkingTime.containsKey(carId)) {
@@ -53,12 +53,10 @@ public class Parking {
                     totalSum += rate;
 
                 }
-//            parkedCars.remove(carId);
             parkingTime.remove(carId);
             System.out.printf("Машина с ID %s убрана из парковки. Общая сумма за парковку: %d\n", carId, totalSum);
             return totalSum;
 
-            // Получаем время въезда машины на парковку
         } catch (CarNotFoundException e) {
             System.out.println(e.getMessage());
             return 0;
