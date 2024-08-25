125ff45c2f8caaf6e7ac3be9c0884f3f6382ff51
Dima <demossml11@gmail.com>
2024-08-25 13:36:56 +0300

    reformat 2

--- src/tasks/Runner.java
+++ src/tasks/Runner.java
@@ -6,22 +6,21 @@ import java.util.HashSet;
 public class Runner {
     public static void main(String[] args) throws CarNotFoundException, CarIsAlreadyParkedException {
 
-        int capacity = 4;
+        int capacity = 10;
 
         Parking parking = new Parking(capacity);
         System.out.println(parking.toString());
 
         parking.park("A", 2);
         parking.park("B", 5);
-        parking.park("B", 5);
+        parking.park("B", 8);
         parking.park("C", 7);
         parking.park("D", 2);
         parking.park("E", 8);
         parking.park("F", 12);
         parking.park("G", 1);
 
-        parking.unpark("B", 5);
+        parking.unpark("Y", 5);
 
-        System.out.println(parking.toString());
     }
 }
